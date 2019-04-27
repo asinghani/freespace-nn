7b103cb1ec0719e27f0ab01ee6628fd316d18c38
Anish <anish.singhani@gmail.com>
2019-04-27 14:27:10 -0700

    vis fixes

--- util/vis.py
+++ util/vis.py
@@ -4,7 +4,7 @@ import numpy as np
 """
 Visualize segmentation output
 """
-def view_seg_map(img, seg, color=(0, 255, 0), alpha=0.4, include_overlay=False):
+def view_seg_map(img, seg, color=(0, 255, 0), alpha=0.4, include_overlay=False, include_mask=False):
     if len(seg.shape) > 2 and seg.shape[2] > 1:
         seg = cv2.cvtColor(seg, cv2.COLOR_BGR2GRAY)
 
@@ -16,11 +16,17 @@ def view_seg_map(img, seg, color=(0, 255, 0), alpha=0.4, include_overlay=False):
     overlay = img.copy()
     overlay[np.where((seg > 0).all(axis = 2))] = color
 
+    overlay2 = img.copy()
+    overlay2[np.where((seg > 0).all(axis = 2))] = color
+    overlay2[overlay2 != color] = (0, 0, 0)
+
     output = img.copy()
     cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
 
     if include_overlay:
         return output, overlay
+    else if include_mask:
+        return output, overlay2
     else:
         return output
 
