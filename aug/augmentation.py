3dea24f0377ff25b116e170a12a55496c74e5818
Anish <anish.singhani@gmail.com>
2019-05-03 20:48:50 -0700

    aug fixes

--- aug/augmentation.py
+++ aug/augmentation.py
@@ -38,11 +38,13 @@ def augment(image, label):
     out = gen.apply_transform(image * 255.0, {"theta": rotationAngle, "shear": shearAngle, "tx": shiftX, "ty": shiftY, "zx": zoom, "zy": zoom, "flip_horizontal": horizFlip, "brightness": brightness})
     outLabel = gen.apply_transform(label * 255.0, {"theta": rotationAngle, "shear": shearAngle, "tx": shiftX, "ty": shiftY, "zx": zoom, "zy": zoom, "flip_horizontal": horizFlip})
 
-    out = cv2.cvtColor(out, cv2.COLOR_RGB2HSV)
-    out[:, :, 0] += np.random.normal(0.0, 6.0)
-    out[:, :, 1] += np.random.normal(0.0, 10.0)
-    out[:, :, 2] += np.random.normal(0.0, 10.0)
-    out = cv2.cvtColor(out, cv2.COLOR_HSV2RGB)
+    out = np.array(cv2.cvtColor(np.array(np.clip(out, 0, 255), dtype=np.uint8), cv2.COLOR_RGB2HSV), np.int16)
+    out[:, :, 0] += np.int16(np.random.normal(0.0, 5.0))
+    out[:, :, 1] += np.int16(np.random.normal(0.0, 8.0))
+    out[:, :, 2] += np.int16(np.random.normal(0.0, 10.0))
+    out[:, :, 0] = np.clip(out[:, :, 0], 0, 179)
+    out = np.clip(out, 0, 255).astype(np.uint8)
+    out = cv2.cvtColor(out, cv2.COLOR_HSV2RGB).astype(np.float32)
 
     return out / 255.0, outLabel / 255.0
 
